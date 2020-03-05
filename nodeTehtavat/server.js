2e3684d07495805812b39c4484aec5802d28ad2b
Oskari Ahonen <ahonenoskari@gmail.com>
2020-03-05 11:02:22 +0200

    noodetehtavia

--- nodeTehtavat/server.js
+++ nodeTehtavat/server.js
@@ -1,28 +1,75 @@
-// var express = require('express');
-// var app = express();
+var express = require('express');
+var app = express();
 
-// app.get('/', function (req, res) {
-//     res.send("Hello world!");
-// });
-
-// var server = app.listen(8081, function () {
-//     var host = server.address().address;
-//     var port = server.address().port;
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
    
-//     console.log("Example app listening at http://%s:%s", host, port);
-// });
-
-// console.log("mo");
+    console.log("Example app listening at http://%s:%s", host, port);
+});
 
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "salakka"
+    password: "salakka",
+    database: "example_db"
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+function getInformationFromDB (callback) {
+
+    // eslint-disable-next-line no-unused-vars
+    con.query('SELECT * FROM location', function(err, result, fields)
+    {
+        if (err) return callback(err);
+        callback(null, result);
+    });
+}
+
+console.log("Call Function");
+getInformationFromDB(function (err, result) {
+    if (err) console.log("Database error!");
+    else {
+        app.get('/', function (req, res) {
+            res.send(result);
+        });
+    }
 });
+
+// async function testi(){
+//     try {
+//         const rivi = await con.query('SELECT * FROM location', function(err, result, fields)
+//         {
+//             if (err) throw err;
+//             const palaute = result;
+//             return palaute; 
+//         });
+//         return rivi;
+//     } catch (err){
+//         console.log("kikki");
+//     }
+// }
+
+// testi().then((x) => function(){
+//     app.get('/', function (req, res) {
+//         res.send(x);
+//     });
+// });
+
+// function asyncAwait() {
+//     let testi = "ase";
+//     testi = con.query('SELECT * FROM location', async function(err, result, fields)
+//     {
+//         if (err) throw err;
+//         const palaute = await result;
+//         return palaute; 
+//     });
+//     return testi;
+// }
+// console.log(testi());
+
+// app.get('/', function (req, res) {
+//     res.send(asyncAwait());
+// });
+// asyncAwait();
